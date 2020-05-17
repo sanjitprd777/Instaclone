d677b9ca53474047cedc6c51c60cb77fc1b347c7
unknown <sanjitpd777@gmail.com>
2020-05-17 12:35:28 +0530

    Added Log in/out feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+	validates :user_id, presence: true
+	belongs_to :user
 	validates :image, presence: true
-	validates :caption, presence: true
+	validates :caption, presence: true, length: { minimum: 1 }
 	has_attached_file :image, styles: { :medium => "640x" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
