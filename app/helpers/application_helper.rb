5e94ea2da0364f34d86974b82da29416636c44ee
unknown <sanjitpd777@gmail.com>
2020-05-17 15:02:55 +0530

    Added CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+	def form_image_select(post)
+	  return image_tag post.image.url(:medium),
+	                   id: 'image-preview',
+	                   class: 'img-responsive' if post.image.exists?
+	  image_tag "placeholder.jpg", id: 'image-preview', class: 'img-responsive'
+	end
+
 	def alert_for(flash_type)
 	  { success: 'alert-success',
 	    error: 'alert-danger',
@@ -6,11 +13,5 @@ module ApplicationHelper
 	    notice: 'alert-info'
 	  }[flash_type.to_sym] || flash_type.to_s
 	end
-	def form_image_select(post)
-	  return image_tag post.image.url(:medium),
-	                   id: 'image-preview',
-	                   class: 'img-responsive' if post.image.exists?
-	  image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
-	end
-
+	
 end
